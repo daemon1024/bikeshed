9ce3eb100c66749637a57303ad2948e8152a1693
daemon1024 <barun1024@gmail.com>
2020-10-21 02:36:23 +0530

    Respond to github events via authorized account
    Co-authored-by: Akshat Agarwal <humancalico@disroot.org>

--- mod.ts
+++ mod.ts
@@ -1,16 +1,38 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
 
 const app = new Application();
 
 const router = new Router();
 
+const ghtoken = Deno.env.get("GH_TOKEN");
+
 router.post(
   "/hello",
-  async ({ request, response }: { request: any; response: any }) => {
+  async ({ request }: { request: any }) => {
     try {
       const body = request.body();
       const data = await body.value;
-      console.log(data);
+      console.log(data.action);
+      if (data.action == "opened") {
+        const msg: string = "thanks for opening an issue.";
+        const reqbody = {
+          "body": msg,
+        };
+        console.log(msg);
+        const ghresp = await fetch(
+          `https://api.github.com/repos/daemon1024/demo/issues/${data.issue.number}/comments`,
+          {
+            method: "POST",
+            headers: {
+              "Accept": "application/vnd.github.v3+json",
+              "Authorization": `token ${ghtoken}`,
+            },
+            body: JSON.stringify(reqbody), // body data type must match "Content-Type" header
+          },
+        );
+        console.log(ghresp);
+      }
     } catch (error) {
       console.log(error);
     }
