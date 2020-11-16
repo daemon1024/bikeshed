f2eb2369c09af03e9cfa437103c4ed6922712541
daemon1024 <barun1024@gmail.com>
2020-11-16 20:29:38 +0530

    refactor and add TODO

--- mod.ts
+++ mod.ts
@@ -8,7 +8,7 @@ const router = new Router();
 const ghtoken = Deno.env.get("GH_TOKEN");
 
 router.post(
-  "/hello",
+  "/recieveghpayload",
   async ({ request }: { request: any }) => {
     try {
       const body = request.body();
@@ -22,6 +22,21 @@ router.post(
   },
 );
 
+const createComment = async (url: string, msg: string) =>
+  await fetch(
+    url,
+    {
+      method: "POST",
+      headers: {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": `token ${ghtoken}`,
+      },
+      body: JSON.stringify({
+        "body": msg,
+      }),
+    },
+  );
+
 const reply: (data: any) => void = async (data) => {
   if (data.issue && data.action === "opened") {
     // An issue is created.
@@ -31,68 +46,30 @@ const reply: (data: any) => void = async (data) => {
       //TODO check if previously created any issue
       msg = "Thanks for opening your first issue.";
     }
-    const reqbody = {
-      "body": msg,
-    };
     console.log(msg);
-    const ghresp = await fetch(
-      data.issue.comments_url,
-      {
-        method: "POST",
-        headers: {
-          "Accept": "application/vnd.github.v3+json",
-          "Authorization": `token ${ghtoken}`,
-        },
-        body: JSON.stringify(reqbody),
-      },
-    );
+    const ghresp = await createComment(data.issue.comments_url, msg);
     console.log(ghresp);
   }
   if (data.pull_request && data.action === "opened") {
     // A PR is made.
     let msg: string = `Thanks for making a PR @${data.pull_request.user.login}`;
     if (data.pull_request.author_association == "NONE") {
+      //TODO check if previously created any PR
       msg = `Thanks for making your first PR @${data.pull_request.user.login}`;
     }
-    const reqbody = {
-      "body": msg,
-    };
-    console.log(msg);
-    const ghresp = await fetch(
-      data.pull_request.issue_url,
-      {
-        method: "POST",
-        headers: {
-          "Accept": "application/vnd.github.v3+json",
-          "Authorization": `token ${ghtoken}`,
-        },
-        body: JSON.stringify(reqbody),
-      },
-    );
+    const ghresp = await createComment(data.pull_request.comments_url, msg);
     console.log(ghresp);
   }
   if (data.pull_request && data.action === "closed") {
-    // A PR is made.
+    // A PR is closed/merged.
+    // TODO differentiate between merge and close.
     let msg: string =
       `Grats your PR is merged now @${data.pull_request.user.login}`;
     if (data.pull_request.author_association == "NONE") {
       msg = `Grats for your first PR @${data.pull_request.user.login}`;
     }
-    const reqbody = {
-      "body": msg,
-    };
     console.log(msg);
-    const ghresp = await fetch(
-      data.pull_request.issue_url,
-      {
-        method: "POST",
-        headers: {
-          "Accept": "application/vnd.github.v3+json",
-          "Authorization": `token ${ghtoken}`,
-        },
-        body: JSON.stringify(reqbody),
-      },
-    );
+    const ghresp = await createComment(data.pull_request.comments_url, msg);
     console.log(ghresp);
   }
 };
