0ba4b15488f3480063598d6ecaddca843b05d550
daemon1024 <barun1024@gmail.com>
2020-11-16 20:10:09 +0530

    check author association

--- mod.ts
+++ mod.ts
@@ -13,7 +13,8 @@ router.post(
     try {
       const body = request.body();
       const data = await body.value;
-      console.log(data);
+      console.log("invoked");
+      // console.log(data);
       reply(data);
     } catch (error) {
       console.log(error);
@@ -24,7 +25,12 @@ router.post(
 const reply: (data: any) => void = async (data) => {
   if (data.issue && data.action === "opened") {
     // An issue is created.
-    const msg: string = "Thanks for opening an issue.";
+    let msg: string = "Thanks for opening an issue.";
+    if (data.issue.author_association == "NONE") {
+      //TODO set in config file
+      //TODO check if previously created any issue
+      msg = "Thanks for opening your first issue.";
+    }
     const reqbody = {
       "body": msg,
     };
@@ -41,10 +47,37 @@ const reply: (data: any) => void = async (data) => {
       },
     );
     console.log(ghresp);
-  } else if (data.pull_request && data.action === "opened") {
+  }
+  if (data.pull_request && data.action === "opened") {
     // A PR is made.
-    const msg: string =
-      `Thanks for making a PR @${data.pull_request.user.login}`;
+    let msg: string = `Thanks for making a PR @${data.pull_request.user.login}`;
+    if (data.pull_request.author_association == "NONE") {
+      msg = `Thanks for making your first PR @${data.pull_request.user.login}`;
+    }
+    const reqbody = {
+      "body": msg,
+    };
+    console.log(msg);
+    const ghresp = await fetch(
+      data.pull_request.issue_url,
+      {
+        method: "POST",
+        headers: {
+          "Accept": "application/vnd.github.v3+json",
+          "Authorization": `token ${ghtoken}`,
+        },
+        body: JSON.stringify(reqbody),
+      },
+    );
+    console.log(ghresp);
+  }
+  if (data.pull_request && data.action === "closed") {
+    // A PR is made.
+    let msg: string =
+      `Grats your PR is merged now @${data.pull_request.user.login}`;
+    if (data.pull_request.author_association == "NONE") {
+      msg = `Grats for your first PR @${data.pull_request.user.login}`;
+    }
     const reqbody = {
       "body": msg,
     };
